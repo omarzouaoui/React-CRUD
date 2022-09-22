32ec27fe510f641588c09381317de4b85cbee1f6
omarzouaoui <zouaouiomar80@gmail.com>
2022-09-22 15:46:59 +0100

    working

---
+++ task-manager-app/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+//components
+import Header from './components/Header';
+import Tasks from './components/Tasks';
+import addTask from './components/AddTask';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
